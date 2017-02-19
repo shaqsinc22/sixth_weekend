c9694421bd3ec1b4af2a351e0e102bf9046bfc65
Shaqsinc <shaeelafsar@gmail.com>
2017-02-19 09:04:18 -0600

    Initial commit

--- algo/prime_number.rb
+++ algo/prime_number.rb
@@ -4,14 +4,37 @@
 # true if the number is prime and false if it is not. A prime number is a number that is only
 # divisible by itself and the number 1.
 
-def is_prime?(number)
+def is_prime?(number)    
+  divisible_3 = number / 3.0
+  divisible_2 = number / 2.0
 
+  if divisible_2 == divisible_2.to_i || divisible_3 == divisible_3.to_i
+    return false
+  else
+    return true
+  end 
 end
 
 # Second, implement the highest_prime_number_under method below. It should accept a number as
 # an argument and return the highest prime number under that number. For example, the 
 # highest prime number under 10 is 7.
 
-def highest_prime_number_under(number)
 
+def highest_prime_number(number)
+  divisible_3 = number / 3.0
+  divisible_2 = number / 2.0
+
+  while number > 0 do
+    if divisible_2 == divisible_2.to_i || divisible_3 == divisible_3.to_i
+      number -= 1
+    else
+      return number
+    end
+  end
 end
+
+
+
+value = 12
+p is_prime?(value)
+p highest_prime_number(value)
--- oop/thesaurus.rb
+++ oop/thesaurus.rb
@@ -10,3 +10,127 @@
 #
 # Part of the challenge is to determine which functionality belongs in the Thesaurus class,
 # and which belongs in the Entry class.
+
+
+class Thesaurus
+  def initialize(entry)
+    @entries = []
+    @entry = entry
+    @entries << @entry
+  end
+
+
+  def save_entry(entry)
+    @entries << entry
+  end
+
+  def add_word
+    puts "Please type a word you would like to add!"
+    word = gets.chomp
+    new_entry = {word: word, synonyms: [], antonyms: []}
+    entry = Entry.new(new_entry)
+    @entries << entry
+    puts "Your new word has been added!"
+  end
+
+  def delete_entry
+   puts "Which number would you like to delete!"
+   view_all_entries
+   choice = gets.chomp.to_i
+   @entries.delete_at(choice)
+   puts "Your entry has been deleted!"
+  end
+
+  def find_synonyms
+    view_all_entries
+    puts "Which number would you like to view the synonyms of?"
+    choice = gets.chomp.to_i
+    puts "Here are the synonyms:"
+    p @entries[choice].synonyms
+  end
+
+  def find_antonyms
+    view_all_entries
+    puts "Which number would you like to view the antonyms of?"
+    choice = gets.chomp.to_i
+    puts "Here are the antonyms:"
+    p @entries[choice].antonyms
+  end
+
+  def add_synonym
+    view_all_entries
+    puts "Which number would you like to add a synonym to?"
+    choice = gets.chomp.to_i
+    puts "Perfect, what is the word you would like to add?"
+    snynonym = gets.chomp
+    @entries[choice].synonyms << snynonym
+    puts "New Synonym has been added, here is the new list:"
+    puts "Synonyms are now: #{@entries[choice].synonyms}"
+  end
+
+  def add_antonym
+    view_all_entries
+    puts "Which number would you like to add a antonym to?"
+    choice = gets.chomp.to_i
+    puts "Perfect, what is the word you would like to add?"
+    antonym = gets.chomp
+    @entries[choice].antonyms << antonym
+    puts "New Antonym has been added, here is the new list:"
+    puts "Antonyms are now: #{@entries[choice].antonyms}"
+  end
+
+  def view_all_entries
+    count = 0
+    @entries.each do |entry|
+      puts "#{count}   Word: '#{entry.word}'"
+      puts "Synonyms: '#{entry.synonyms}'"
+      puts "Antonyms: '#{entry.antonyms}'"
+      puts "-------------------------------"
+      count += 1
+    end
+  end
+end
+
+
+class Entry < Thesaurus
+  attr_accessor :word, :synonyms, :antonyms
+  def initialize(input_options)
+    @word = input_options[:word]
+    @synonyms = input_options[:synonyms]
+    @antonyms = input_options[:antonyms]
+  end
+end
+
+
+
+entry = Entry.new(
+                  word: "good",
+                  synonyms: ["well", "right"],
+                  antonyms: ["bad", "evil"]
+                  )
+
+second_entry = Entry.new(
+                        word: "shy",
+                        synonyms: ["afraid", "scared"],
+                        antonyms: ["outgoing", "confident"]
+                        )
+
+third_entry = Entry.new(
+                        word: "happy",
+                        synonyms: ["excited", "positive"],
+                        antonyms: ["upset", "sad"]
+                        )
+
+thesaurus = Thesaurus.new(entry)
+
+thesaurus.save_entry(second_entry)
+thesaurus.save_entry(third_entry)
+# thesaurus.add_word
+# thesaurus.find_synonyms
+# thesaurus.find_antonyms
+# thesaurus.add_synonym
+# thesaurus.add_antonym
+# thesaurus.delete_entry
+# thesaurus.view_all_entries
+
+
